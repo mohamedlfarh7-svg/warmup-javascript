1c890a7d40f01139c91ab760209c26c22c41fa65
mohamed <mohamedlfarh7@gmail.com>
2026-09-02 10:58:36 +0100

    finish the 7th exercise

---
+++ exercice-07.js
@@ -0,0 +1,34 @@
+function genererSlug(titre) {
+    return titre
+        .trim()
+        .toLowerCase()
+        .split(" ")
+        .filter(mot => mot !== "")
+        .join("-");
+}
+
+function compterMots(phrase) {
+    return phrase
+        .trim()
+        .split(" ")
+        .filter(mot => mot !== "")
+        .length;
+}
+
+function initiales(nomComplet) {
+    return nomComplet
+        .trim()
+        .split(" ")
+        .filter(mot => mot !== "")
+        .map(mot => mot[0].toUpperCase())
+        .join(".") + ".";
+}
+
+const titre = " Mon Premier Projet MERN ";
+const phrase = "Le JavaScript est la base du stack MERN";
+const nomComplet = "amina el idrissi";
+
+console.log(genererSlug(titre));
+console.log(compterMots(phrase));
+console.log(initiales(nomComplet));
+console.log(genererSlug(" Sprint 1 "));
